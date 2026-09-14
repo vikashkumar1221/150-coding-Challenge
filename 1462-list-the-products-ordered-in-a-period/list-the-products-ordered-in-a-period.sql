dac5d435ce6ccbaa7a613747c3b500a59a547b29
vikashkumar1221 <vikashkumarjangir30@gmail.com>
2026-09-15 04:27:08 +0530

    Time: 818 ms (53.94%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -1,7 +1,6 @@
 # Write your MySQL query statement below
 SELECT DISTINCT p.product_name, s.sumOfUnit AS unit
-FROM Products p JOIN Orders o ON p.product_id = o.product_id 
-JOIN (
+FROM Products p JOIN (
     SELECT product_id, SUM(unit) AS sumOfUnit, order_date
     FROM Orders 
     WHERE order_date BETWEEN '2020-02-01' AND '2020-02-29'
